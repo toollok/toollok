1bb58aab40fe9f05591cbddf2e442e4d4b6a908c
ToolLok <support@toollok.com>
2026-09-15 20:47:52 +0530

    Initial commit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
                allow: '/',
                disallow: ['/api/'],
           },
-          sitemap: 'https://toollok.com/sitemap.xml',
+          sitemap: 'https://www.toollok.com/sitemap.xml',
      };
 }
